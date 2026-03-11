fb672845c194686da477aa5f96e1c8bbc032298f
ilmasheva-eliza <ilmeliza36@gmai.com>
2026-03-11 20:30:25 +0500

    Разработанное перенаправление на страницу с предметами

---
+++ praktika22 готовая!!!!!!!!!/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace praktika22.Controllers
+{
+	public class HomeController: Controller
+	{
+		public RedirectResult Index() {
+			return Redirect("/Items/List");
+		}
+	}
+}
